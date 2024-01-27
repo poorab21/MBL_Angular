d55a5af6f46b7d009e92f8cf26be375160d3ac13
poorab21 <111352184+poorab21@users.noreply.github.com>
2024-01-27 11:37:43 +0500

    Invalid Product ID error toastr added

--- src/assets/Services/product-guard.service.ts
+++ src/assets/Services/product-guard.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { ProductService } from './product.service';
 import { Observable } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductGuardService implements CanActivate {
 
-  constructor( private router: Router , private productService: ProductService ) {}
+  constructor( private toastrService: ToastrService , private router: Router , private productService: ProductService ) {}
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
     return new Promise((resolve,reject) => {
@@ -16,7 +17,12 @@ export class ProductGuardService implements CanActivate {
       this.productService.getProducts().subscribe((products: any) => {
         const result = products.find((product: any) => product.id == route.params['id']);
 
-        if(result == undefined) { this.router.navigate(['productNotFound']); resolve(true); }
+        if(result == undefined) {
+          this.toastrService.error("Product with specified ID not found");
+          this.router.navigate(['onboarding/product/list']); 
+          resolve(true); 
+        }
+
         resolve(true)
       })
     });
